1ebed0a6289482b09b4c84199e5a922b9de5ca16
Shawn DelPercio <sdelperc@gmail.com>
2020-08-10 15:36:04 -0400

    added cupcake svg to bakery

--- src/components/Bakery.js
+++ src/components/Bakery.js
@@ -1,6 +1,14 @@
 import React from "react";
+// components and media
+import { ReactComponent as Cupcake } from "../media/cupcake-svg.svg";
+import { ReactComponent as Toaster } from "../media/toaster-svg.svg";
+import { ReactComponent as Oven } from "../media/oven-svg.svg";
+import { ReactComponent as IndustrialOven } from "../media/industrial-oven-svg.svg";
+import { ReactComponent as Friends } from "../media/friends-svg.svg";
+import { ReactComponent as Chef } from "../media/chef-svg.svg";
+import { ReactComponent as CupcakeGod } from "../media/cupcake-god-svg.svg";
 // styles
-import { Flex, Text, Button } from "@chakra-ui/core";
+import { Flex, Text, Button, Box } from "@chakra-ui/core";
 
 function Bakery({
   cupcakes,
@@ -13,22 +21,33 @@ function Bakery({
   cupcakeGods,
 }) {
   return (
-    <Flex>
-      <Flex>
+    <Flex direction="column" m="0 auto">
+      <Flex direction="column" align="center" textAlign="center">
+        <Button
+          as={Cupcake}
+          onClick={bakeCupcakes}
+          bg="None"
+          rounded="50%"
+          cursor="pointer"
+          h="150px"
+          w="150px"
+        />
         <Text>{cupcakes}</Text>
-        <Button onClick={bakeCupcakes}>Bake!</Button>
+        <Text>⬆ Bake! ⬆</Text>
       </Flex>
       <Flex>
-        <Flex>
+        <Box textAlign="center">
+          <Toaster style={{ height: "75px", width: "75px" }} />
           <Text>Toasters</Text>
           <Text>{toasters}</Text>
-        </Flex>
-        <Flex>
+        </Box>
+        <Box textAlign="center">
+          <Oven style={{ height: "75px", width: "75px" }} />
           <Text>Ovens</Text>
           <Text>{ovens}</Text>
-        </Flex>
+        </Box>
         <Flex>
-          <Text>Industrial</Text>
+          <Text>Industrial Ovens</Text>
           <Text>{industrialOvens}</Text>
         </Flex>
         <Flex>
