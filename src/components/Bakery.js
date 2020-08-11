ace451e3154bdc70a1b0e43180ee667055f8cee3
Shawn DelPercio <sdelperc@gmail.com>
2020-08-11 16:55:41 -0400

    fixed spacing for bakery page

--- src/components/Bakery.js
+++ src/components/Bakery.js
@@ -21,7 +21,7 @@ function Bakery({
   chefs,
   cupcakeGods,
 }) {
-  const [resetScale, setResetScale] = useState(false);
+  // const [resetScale, setResetScale] = useState(false);
 
   // TODO: react-spring on click for baking cupcakes
   // const props = useSpring({
@@ -34,11 +34,16 @@ function Bakery({
 
   return (
     <Flex direction="column" m="0 auto">
-      <Flex direction="column" align="center" textAlign="center">
+      <Flex
+        direction="column"
+        align="center"
+        textAlign="center"
+        mb={["2rem", "4rem"]}
+      >
         <Button
           as={Cupcake}
           onClick={bakeCupcakes}
-          _hover={{ transform: "scale(1.25, 1.25)" }}
+          _hover={{ transform: "scale(1.2, 1.2)" }}
           _focus={{
             boxShadow:
               "0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)",
@@ -46,40 +51,56 @@ function Bakery({
           bg="None"
           rounded="50%"
           cursor="pointer"
-          h="150px"
-          w="150px"
+          h="200px"
+          w="200px"
+          mb="1rem"
         />
         <Text>{cupcakes}</Text>
         <Text>⬆ Bake! ⬆</Text>
       </Flex>
-      <Flex>
-        <Box textAlign="center">
-          <Toaster style={{ height: "75px", width: "75px" }} />
+      <Flex
+        direction={["column", "row"]}
+        justify="space-between"
+        align="center"
+        w="100%"
+        flexWrap="wrap"
+        m="0 auto"
+      >
+        <Box textAlign="center" w={["100%", "50%", "33%"]} mb="1rem">
+          <Toaster
+            style={{ height: "75px", width: "75px", margin: "0 auto" }}
+          />
           <Text>Toasters</Text>
           <Text>{toasters}</Text>
         </Box>
-        <Box textAlign="center">
-          <Oven style={{ height: "75px", width: "75px" }} />
+        <Box textAlign="center" w={["100%", "50%", "33%"]} mb="1rem">
+          <Oven style={{ height: "75px", width: "75px", margin: "0 auto" }} />
           <Text>Ovens</Text>
           <Text>{ovens}</Text>
         </Box>
-        <Box textAlign="center">
-          <IndustrialOven style={{ height: "75px", width: "75px" }} />
+        <Box textAlign="center" w={["100%", "50%", "33%"]} mb="1rem">
+          <IndustrialOven
+            style={{ height: "75px", width: "75px", margin: "0 auto" }}
+          />
           <Text>Industrial Ovens</Text>
           <Text>{industrialOvens}</Text>
         </Box>
-        <Box textAlign="center">
-          <Friends style={{ height: "75px", width: "75px" }} />
+        <Box textAlign="center" w={["100%", "50%", "33%"]} mb="1rem">
+          <Friends
+            style={{ height: "75px", width: "75px", margin: "0 auto" }}
+          />
           <Text>Friends</Text>
           <Text>{friends}</Text>
         </Box>
-        <Box textAlign="center">
-          <Chef style={{ height: "75px", width: "75px" }} />
+        <Box textAlign="center" w={["100%", "50%", "33%"]} mb="1rem">
+          <Chef style={{ height: "75px", width: "75px", margin: "0 auto" }} />
           <Text>Chefs</Text>
           <Text>{chefs}</Text>
         </Box>
-        <Box textAlign="center">
-          <CupcakeGod style={{ height: "75px", width: "75px" }} />
+        <Box textAlign="center" w={["100%", "50%", "33%"]} mb="1rem">
+          <CupcakeGod
+            style={{ height: "75px", width: "75px", margin: "0 auto" }}
+          />
           <Text>Cupcake Gods</Text>
           <Text>{cupcakeGods}</Text>
         </Box>
