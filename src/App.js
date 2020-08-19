5dea6ae72f2f13d43a61fb39803ea6097fc071ae
Shawn DelPercio <sdelperc@gmail.com>
2020-08-19 16:12:04 -0400

    added lookup for item purchases; fixed item purchase function

--- src/App.js
+++ src/App.js
@@ -18,14 +18,6 @@ function App() {
   const [chefs, setChefs] = useState(0);
   const [cupcakeGods, setCupcakeGods] = useState(0);
 
-  // TODO: purchase state
-  // const [toastersCost, setToastersCost] = useState(0);
-  // const [ovensCost, setOvensCost] = useState(0);
-  // const [industrialOvensCost, setIndustrialOvensCost] = useState(0);
-  // const [friendsCost, setFriendsCost] = useState(0);
-  // const [chefsCost, setChefsCost] = useState(0);
-  // const [cupcakeGodsCost, setCupcakeGodsCost] = useState(0);
-
   // setup item purchasing functions
   const purchaseLookup = {};
   purchaseLookup.toasters = () => setToasters((toasters) => toasters + 1);
@@ -37,12 +29,58 @@ function App() {
   purchaseLookup.cupcakeGods = () =>
     setCupcakeGods((cupcakeGods) => cupcakeGods + 1);
 
-  // TODO: purchase Item function, decrease cupcakes, increase item count, increase cost
-  // function purchaseItem(e, item) {
-  //   e.preventDefault();
-  //   purchaseLookup[item]();
-  //   setCupcakes((cupcakes) => cupcakes - cost);
-  // }
+  // purchase state
+  const [toastersCost, setToastersCost] = useState(0);
+  const [ovensCost, setOvensCost] = useState(0);
+  const [industrialOvensCost, setIndustrialOvensCost] = useState(0);
+  const [friendsCost, setFriendsCost] = useState(0);
+  const [chefsCost, setChefsCost] = useState(0);
+  const [cupcakeGodsCost, setCupcakeGodsCost] = useState(0);
+
+  // setup item costs and increases
+  const itemCostLookup = {};
+  itemCostLookup.toasters = () => {
+    const prevCost = toastersCost;
+    setToastersCost((toasterCost) => toasterCost + 50 + 0.1 * cupcakes);
+    return prevCost;
+  };
+  itemCostLookup.ovens = () => {
+    const prevCost = ovensCost;
+    setOvensCost((ovensCost) => ovensCost + 100 + 0.2 * cupcakes);
+    return prevCost;
+  };
+  itemCostLookup.industrialOvens = () => {
+    const prevCost = industrialOvensCost;
+    setIndustrialOvensCost(
+      (industrialOvensCost) => industrialOvensCost + 200 + 0.3 * cupcakes
+    );
+    return prevCost;
+  };
+  itemCostLookup.friends = () => {
+    const prevCost = friendsCost;
+    setFriendsCost((friendsCost) => friendsCost + 100 + 0.2 * cupcakes);
+    return prevCost;
+  };
+  itemCostLookup.chefs = () => {
+    const prevCost = chefsCost;
+    setChefsCost((chefsCost) => chefsCost + 300 + 0.4 * cupcakes);
+    return prevCost;
+  };
+  itemCostLookup.cupcakeGods = () => {
+    const prevCost = cupcakeGodsCost;
+    setCupcakeGodsCost(
+      (cupcakeGodsCost) => cupcakeGodsCost + 1000 + 0.8 * cupcakes
+    );
+    return prevCost;
+  };
+
+  // purchase item function: increase item count, increase cost, decrease cupcakes
+  function purchaseItem(e, item) {
+    e.preventDefault();
+    purchaseLookup[item]();
+    const itemCost = itemCostLookup[item]();
+    setCupcakes((cupcakes) => cupcakes - itemCost);
+  }
 
   function bakeCupcakes() {
     let total = 1;
@@ -54,15 +92,18 @@ function App() {
   }
 
   // increase cupcakes based on items
-  useInterval(() => {
-    let total = 0;
-    total += 5 * friends;
-    total += 10 * chefs;
-    total += 100 * cupcakeGods;
+  useInterval(
+    () => {
+      let total = 0;
+      total += 5 * friends;
+      total += 10 * chefs;
+      total += 100 * cupcakeGods;
 
-    setCupcakes((cupcakes) => cupcakes + total);
-    document.title = `${cupcakes} Cupcakes Baked`;
-  }, 1000);
+      setCupcakes((cupcakes) => cupcakes + total);
+      document.title = `${cupcakes} Cupcakes Baked`;
+    },
+    cupcakes ? 1000 : null
+  );
 
   return (
     <Flex flexDirection="column">
