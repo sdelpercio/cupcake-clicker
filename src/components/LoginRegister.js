6e19a1787c31523b65b105fa1b6c85ba5a368b29
Shawn DelPercio <sdelperc@gmail.com>
2020-09-01 16:40:06 -0400

    validation functions added

--- src/components/LoginRegister.js
+++ src/components/LoginRegister.js
@@ -12,87 +12,101 @@ import {
 } from "@chakra-ui/core";
 
 function LoginRegister({ user, setUser }) {
-  //   const { register, handleSubmit, errors, formState } = useForm();
-  //   const onSubmit = (data) => console.log(data);
+  const { register, handleSubmit, errors, formState, getValues } = useForm();
+  const onSubmit = (data) => console.log(data);
 
   // TODO: validation
-  //   function validateUsername(value) {
-  //       let error;
-  //       if (!value) {
-  //           error = "Username is required";
-  //       } else if (value.length > 15 || value.length < 2) {
-  //           error = "Username must be 2 - 15 characters long"
-  //       }
-  //       return error || true;
-  //   }
-  //   function validatePassword(value) {
-  //       let error;
-  //       const expression = ^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z])$
-  //       if (!value) {
-  //           error = "Password is required";
-  //       } else if (value.length < 8) {
-  //           error = "Password must be at least 8 characters long"
-  //       } else if (expression.test(value) === false) {
-  //           error = "Password must include "
-  //       }
-  //   }
+  function validateUsername(value) {
+    let error;
+    if (!value) {
+      error = "Username is required";
+    } else if (value.length > 15 || value.length < 2) {
+      error = "Username must be 2 - 15 characters long";
+    }
+    return error || true;
+  }
+  function validatePassword(value) {
+    let error;
+    const expression = new RegExp(
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[^da-zA-Z])$"
+    );
+    if (!value) {
+      error = "Password is required";
+    } else if (value.length < 8) {
+      error = "Password must be at least 8 characters long";
+    } else if (expression.test(value) === false) {
+      error =
+        "Password must include a capital letter, number, and special character";
+    }
+    return error || true;
+  }
+  function validateRetypePassword(value) {
+    let error;
+    const top_password = getValues("Password");
+    if (!value) {
+      error = "Must re-type your password";
+    } else if (value !== top_password) {
+      error = "Passwords must match";
+    }
+    return error || true;
+  }
 
-  //   return (
-  //     <Flex direction="column" m="0 auto">
-  //       <Text>
-  //         {user.name === "Aspiring Baker"
-  //           ? "Register to save your cupcakes!"
-  //           : `Your Cupcakes are safe with us, ${user.name} :)`}
-  //       </Text>
-  //       {user.name === "Aspiring Baker" ? (
-  //         //   REGISTER
-  //         <form onSubmit={handleSubmit(onSubmit)}>
-  //           <FormControl
-  //             isInvalid={
-  //               errors.Username || errors.Password || errors.RetypePassword
-  //             }
-  //           >
-  //             <FormLabel htmlFor="Username">Username</FormLabel>
-  //             <Input
-  //               type="text"
-  //               placeholder="Username"
-  //               name="Username"
-  //               ref={register({})}
-  //             />
-  //             <FormErrorMessage>
-  //               {errors.Username && errors.Username.message}
-  //             </FormErrorMessage>
-  //             <FormLabel htmlFor="Password">Password</FormLabel>
-  //             <Input
-  //               type="text"
-  //               placeholder="Password"
-  //               name="Password"
-  //               ref={register({})}
-  //             />
-  //             <FormErrorMessage>
-  //               {errors.Password && errors.Password.message}
-  //             </FormErrorMessage>
-  //             <FormLabel htmlFor="RetypePassword">Re-type Password</FormLabel>
-  //             <Input
-  //               type="text"
-  //               placeholder="Re-type Password"
-  //               name="RetypePassword"
-  //               ref={register({})}
-  //             />
-  //             <FormErrorMessage>
-  //               {errors.RetypePassword && errors.RetypePassword.message}
-  //             </FormErrorMessage>
-  //           </FormControl>
-  //           <Button isLoading={formState.isSubmitting} type="submit">
-  //             Submit
-  //           </Button>
-  //         </form>
-  //       ) : (
-  //         <Flex>Login</Flex>
-  //       )}
-  //     </Flex>
-  //   );
-  return <Text m="0 auto">Coming Soon</Text>;
+  return (
+    <Flex direction="column" m="0 auto">
+      <Text>
+        {user.name === "Aspiring Baker"
+          ? "Register to save your cupcakes!"
+          : `Your Cupcakes are safe with us, ${user.name} :)`}
+      </Text>
+      {user.name === "Aspiring Baker" ? (
+        //   REGISTER
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl
+            isInvalid={
+              errors.Username || errors.Password || errors.RetypePassword
+            }
+          >
+            <FormLabel htmlFor="Username">Username</FormLabel>
+            <Input
+              type="text"
+              placeholder="Username"
+              name="Username"
+              ref={register({})}
+            />
+            <FormErrorMessage>
+              {errors.Username && errors.Username.message}
+            </FormErrorMessage>
+            <FormLabel htmlFor="Password">Password</FormLabel>
+            <Input
+              type="text"
+              placeholder="Password"
+              name="Password"
+              ref={register({})}
+            />
+            <FormErrorMessage>
+              {errors.Password && errors.Password.message}
+            </FormErrorMessage>
+            <FormLabel htmlFor="RetypePassword">Re-type Password</FormLabel>
+            <Input
+              type="text"
+              placeholder="Re-type Password"
+              name="RetypePassword"
+              ref={register({})}
+            />
+            <FormErrorMessage>
+              {errors.RetypePassword && errors.RetypePassword.message}
+            </FormErrorMessage>
+          </FormControl>
+          <Button isLoading={formState.isSubmitting} type="submit">
+            Submit
+          </Button>
+        </form>
+      ) : (
+        <Flex>Login</Flex>
+      )}
+    </Flex>
+  );
+  // return <Text m="0 auto">Coming Soon</Text>;
 }
 
 export default LoginRegister;
